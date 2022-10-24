06fab5a4de528c96b5f21f76d43bb944abd237b8
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-10-24 17:59:50 -0300

    rotas veterinario

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { Router } from "express";
 
 import clienteController from "../controllers/cliente/clienteController";
 import vacinaController from "../controllers/vacina/vacinaController";
+import veterinarioController from "../controllers/veterinario/veterinarioController";
+import funcionarioController from "../controllers/funcionario/funcionarioController";
 
 const router = Router();
 
@@ -16,5 +18,11 @@ router.get("/vacinas", vacinaController.findAllVacina);
 router.post("/vacina", vacinaController.createVacina);
 router.put("/vacina/:id", vacinaController.updateVacina);
 router.delete("/vacina/:id", vacinaController.deleteVacina);
+
+// veterinario
+router.get("/veterinarios", veterinarioController.findAllVeterinario);
+router.post("/veterinario", veterinarioController.createVeterinario);
+router.put("/veterinario/:id", veterinarioController.updateVeterinario);
+router.delete("/veterinario/:id", veterinarioController.deleteVeterinario);
 
 export { router };
