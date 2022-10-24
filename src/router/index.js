d116f1b4bb73f1754321971995fb64dbd3c13ec1
Rubens Lima Ferreira <62750539+RubensLFerreira@users.noreply.github.com>
2022-10-24 16:17:17 -0300

    rotas de vacina e cliente criadas

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
 
 import clienteController from "../controllers/cliente/clienteController";
+import vacinaController from "../controllers/vacina/vacinaController";
 
 const router = Router();
 
+// cliente
 router.get("/clientes", clienteController.findAllCliente);
+router.post("/cliente", clienteController.createCliente);
+router.put("/cliente/:id", clienteController.updateCliente);
+router.delete("/cliente/:id", clienteController.deleteCliente);
+
+// vacina
+router.get("/vacinas", vacinaController.findAllVacina);
+router.post("/vacina", vacinaController.createVacina);
+router.put("/vacina/:id", vacinaController.updateVacina);
+router.delete("/vacina/:id", vacinaController.deleteVacina);
 
 export { router };
